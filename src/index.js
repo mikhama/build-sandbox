418c6e8448f065f8dc544aeb4b986ded617f2c95
Dzmitry Tsebruk <mikhama@tut.by>
2020-07-19 22:13:39 +0300

    feat: [14-169] learn about loaders

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import Log from './log';
 import Calc from './calc';
 
+import img from './hello.png';
+
 const log = new Log();
 const calc = new Calc();
 
 log.log(calc.add(1, 2, 3));
+
+const element = document.createElement('img');
+element.src = img;
+document.body.appendChild(element);
