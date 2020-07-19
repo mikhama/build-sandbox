7d95edb999373c2046a58fc45a916ff05306ac87
Dzmitry Tsebruk <mikhama@tut.by>
2020-07-19 22:44:40 +0300

    feat: [14-171] learn how to configure loaders for different file types

--- webpack.config.js
+++ webpack.config.js
@@ -3,14 +3,30 @@ module.exports = {
 
   module: {
     rules: [
+
+      // Loading images
       {
-        test: /\.png$/,
+        test: /\.(png|jpg|jpeg|gif|ico)$/,
         use: [
           {
             loader: 'file-loader',
             options: {
               outputPath: 'images',
               name: '[name]-[sha1:hash:7].[ext]'
+            }
+          }
+        ]
+      },
+
+      // Loading fonts
+      {
+        test: /\.(ttf|oft|eot|woff|woff2)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'fonts',
+              name: '[name].[ext]'
             }
           }
         ]
