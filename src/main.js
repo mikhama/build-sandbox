7d770148528a8f92b94340feb3a9788f4db54d07
Dzmitry Tsebruk <mikhama@tut.by>
2020-07-19 16:01:17 +0300

    feat: [13-164] learn how to work with jsx

--- src/main.js
+++ src/main.js
@@ -1,12 +1,6 @@
-class App {
-  run = async (name = 'World') => {
-    console.log(`Hello ${name}`);
-    console.log([1, 2, [3, 4]].flat());
-  }
-}
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-const app = new App();
-app
-  .run()
-  .then(() => console.log('done'))
-  .catch(() => console.log('Error!'));
+const App = () => <p>Hello</p>;
+
+ReactDOM.render(<App />, document.getElementById('root'));
