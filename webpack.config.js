5a2b93bd1ef6f87fe8cb2dd9ab4dd6ef15532332
Dzmitry Tsebruk <mikhama@tut.by>
2020-07-20 09:34:12 +0300

    feat: [14-180] add configuration for prod

--- webpack.config.js
+++ webpack.config.js
@@ -1,80 +1,103 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-  mode: 'development',
+module.exports = (env = {}) => {
+  const { mode = 'development' } = env;
 
-  module: {
-    rules: [
+  const isProd = mode === 'production';
+  const isDev = mode === 'development';
 
-      // Loading javascript
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader'
-      },
+  const getStyleLoaders = () => ([
+    isProd ? MiniCssExtractPlugin.loader : 'style-loader',
+    'css-loader'
+  ]);
 
-      // Loading images
-      {
-        test: /\.(png|jpg|jpeg|gif|ico)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              outputPath: 'images',
-              name: '[name]-[sha1:hash:7].[ext]'
+  const getPlugins = () => {
+    const plugins = [
+      new HtmlWebpackPlugin({
+        title: 'Hello World',
+        buildTime: new Date().toString(),
+        template: 'public/index.html'
+      })
+    ];
+
+    if (isProd) {
+      plugins.push(
+        new MiniCssExtractPlugin({
+          filename: 'main-[hash:8].css'
+        })
+      );
+    }
+
+    return plugins;
+  };
+
+  return {
+    mode: isProd ? 'production' : isDev && 'development',
+
+    output: {
+      filename: isProd ? 'main-[hash:8].js' : undefined
+    },
+
+    module: {
+      rules: [
+
+        // Loading javascript
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader'
+        },
+
+        // Loading images
+        {
+          test: /\.(png|jpg|jpeg|gif|ico)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                outputPath: 'images',
+                name: '[name]-[sha1:hash:7].[ext]'
+              }
             }
-          }
-        ]
-      },
+          ]
+        },
 
-      // Loading fonts
-      {
-        test: /\.(ttf|oft|eot|woff|woff2)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              outputPath: 'fonts',
-              name: '[name].[ext]'
+        // Loading fonts
+        {
+          test: /\.(ttf|oft|eot|woff|woff2)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                outputPath: 'fonts',
+                name: '[name].[ext]'
+              }
             }
-          }
-        ]
-      },
+          ]
+        },
 
-      // Loading CSS
-      {
-        test: /\.(css)$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader'
-        ]
-      },
+        // Loading CSS
+        {
+          test: /\.(css)$/,
+          use: getStyleLoaders()
+        },
 
-      // Loading SASS/SCSS
-      {
-        test: /\.(s[ca]ss)$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'sass-loader'
-        ]
-      }
-    ]
-  },
+        // Loading SASS/SCSS
+        {
+          test: /\.(s[ca]ss)$/,
+          use: [
+            ...getStyleLoaders(),
+            'sass-loader'
+          ]
+        }
+      ]
+    },
 
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Hello World',
-      buildTime: new Date().toString(),
-      template: 'public/index.html'
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'main-[hash:8].css'
-    })
-  ],
+    plugins: getPlugins(),
 
-  devServer: {
-    open: true
-  }
-};
+    devServer: {
+      open: true
+    }
+  };
+}
