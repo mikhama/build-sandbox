2da7b4f07af60a9b6fc8ae3cce0737f9f5c305b3
Dzmitry Tsebruk <mikhama@tut.by>
2020-07-20 09:05:53 +0300

    feat: [14-177] learn how to use templates in HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -64,6 +64,8 @@ module.exports = {
 
   plugins: [
     new HtmlWebpackPlugin({
+      title: 'Hello World',
+      buildTime: new Date().toString(),
       template: 'public/index.html'
     })
   ]
